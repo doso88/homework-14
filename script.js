d1484b22b0c4eba626cd7b2d29a1f110a1c86fe0
doso88 <102288225+doso88@users.noreply.github.com>
2024-04-29 11:55:47 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+/*1*/
+let array =[65, 27, 135, 222, 18]
+let sum = (array[0]+array[1]+array[2]+array[3]+array[4])
+console.log(sum)
+/*2*/
+let person = [{
+        name:"John",
+        age:27,
+        adress:"New-York",
+    },
+    {
+        name:"Bob",
+        age:32,
+        adress:"Las-Vegas",
+    },
+    {
+        name:"Lucas",
+        age:45,
+        adress:"Paris",
+    },
+    {
+        name:"Ann",
+        age:18,
+        adress:"London",
+    },
+    {
+        name:"Nick",
+        age:17,
+        adress:"Rome",
+    },
+    {
+        name:"Barney",
+        age:33,
+        adress:"Vanquver",
+    }
+]
+
+/*3*/
+console.log(person[0].name)
+console.log(person[0].age)
+console.log(person[0].adress)
+
+/*ბევრი ვიწვალე მაგრამ დანარჩენი ვერანაირად ვერ გავაკეთე*/
